45ce0108f1d42da694c17be38659ceb910cd1146
Lyb-code <lyb62051169@163.com>
2021-12-05 04:55:44 -0800

    Pass 64x64 Matrix case in PartB

--- CacheLab/cachelab-handout/trans.c
+++ CacheLab/cachelab-handout/trans.c
@@ -56,6 +56,51 @@ void transpose_submit(int M, int N, int A[N][M], int B[M][N])
                 }
             }
         }
+    } else if (M == 64) { // 64 x 64
+        for (i = 0; i < N; i += 8) {
+            for (j = 0; j < M; j += 8) {
+                for (k = 0; k < 4; k++) { //First 4 rows of A
+                    // A top right
+                    a = A[i+k][j];
+                    b = A[i+k][j+1];
+                    c = A[i+k][j+2];
+                    d = A[i+k][j+3];
+                    // A top left
+                    e = A[i+k][j+4];
+                    f = A[i+k][j+5];
+                    g = A[i+k][j+6];
+                    h = A[i+k][j+7];
+                    // B top left
+                    B[j][i+k] = a;
+                    B[j+1][i+k] = b;
+                    B[j+2][i+k] = c;
+                    B[j+3][i+k] = d;
+                    // B top right
+                    B[j][i+k+4] = e;
+                    B[j+1][i+k+4] = f;
+                    B[j+2][i+k+4] = g;
+                    B[j+3][i+k+4] = h;
+                }
+                for (k = 0; k < 4; k++) { //Last 4 rows of A
+                    //copy 2 columns of A;
+                    a = A[i+4][j+k], e = A[i+4][j+k+4];
+                    b = A[i+5][j+k], f = A[i+5][j+k+4];
+                    c = A[i+6][j+k], g = A[i+6][j+k+4];
+                    d = A[i+7][j+k], h = A[i+7][j+k+4];
+                    // swap "B top right" with first column
+                    s = B[j+k][i+4], B[j+k][i+4] = a, a = s;
+                    s = B[j+k][i+5], B[j+k][i+5] = b, b = s;
+                    s = B[j+k][i+6], B[j+k][i+6] = c, c = s;
+                    s = B[j+k][i+7], B[j+k][i+7] = d, d = s;
+                    // Add "2 columns of A" to Bottom of B;
+                    B[j+k+4][i] = a, B[j+k+4][i+4] = e;
+                    B[j+k+4][i+1] = b, B[j+k+4][i+1+4] = f;
+                    B[j+k+4][i+2] = c, B[j+k+4][i+2+4] = g;
+                    B[j+k+4][i+3] = d, B[j+k+4][i+3+4] = h;
+                }
+                
+            }
+        }
     }
 }
 
